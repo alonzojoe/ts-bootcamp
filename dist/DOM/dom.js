c1004d6db66af8fc0f14fc80e17497a7c256c525
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-05-26 09:49:17 +0800

    working with forms

--- dist/DOM/dom.js
+++ dist/DOM/dom.js
@@ -1,7 +1,17 @@
 "use strict";
 const btn = document.getElementById('btn');
 const input = document.getElementById('todo-input');
-btn.addEventListener('click', () => {
-    alert(input.value);
-    input.value = "";
-});
+const form = document.querySelector('#form-action');
+//button event listener
+// btn.addEventListener('click', () => {
+//     if (input.value === "") return;
+//     alert(input.value)
+//     input.value = ""
+// })
+const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('form subbmited');
+};
+form.addEventListener('submit', handleSubmit);
+let mystery = "5213";
+const numChars = mystery.length;
--- src/DOM/dom.ts
+++ src/DOM/dom.ts
@@ -1,9 +1,21 @@
 const btn = document.getElementById('btn')! as HTMLButtonElement;
 const input = document.getElementById('todo-input')! as HTMLInputElement;
+const form = document.querySelector('#form-action')! as HTMLFormElement;
 
+//button event listener
+// btn.addEventListener('click', () => {
+//     if (input.value === "") return;
+//     alert(input.value)
+//     input.value = ""
+// })
 
+const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+    alert('form subbmited')
+}
 
-btn.addEventListener('click', () => {
-    alert(input.value)
-    input.value = ""
-})
+form.addEventListener('submit', handleSubmit)
+
+let mystery: unknown = "5213"
+
+const numChars = (mystery as string).length
