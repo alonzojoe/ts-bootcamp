fc9e9e394d187b3c725e7fbd3f9759f43de1f369
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-05-27 14:57:35 +0800

    type narrowing using in operator and instanceof

--- src/type-narrowing/typeNarrowing.ts
+++ src/type-narrowing/typeNarrowing.ts
@@ -1,3 +1,4 @@
+console.clear()
 console.log('Type Narrowing')
 
 //Typeof Guard
@@ -43,3 +44,62 @@ const demoFunction = (x: StringNum, y: StringBool) => {
 
 console.log(demoFunction('a', 'a'))
 
+//Narrowing with `in` Operator
+interface CinemaMovie {
+    title: string;
+    duration: number;
+}
+
+interface TVShow {
+    title: string;
+    numEpisodes: number;
+    episodeDuration: number;
+}
+
+const getRunTime = (media: CinemaMovie | TVShow) => {
+    if ("duration" in media) {
+        return media.duration
+    }
+    return media.numEpisodes * media.episodeDuration
+}
+
+//instanceof Narrowing
+class Vehicle { }
+class Car {
+    constructor(public brand: string, public model: string) { }
+    getInfo() {
+        return `${this.brand} ${this.model}`
+    }
+}
+
+class Honda extends Car {
+    constructor(public brand: string, public model: string) {
+        super(brand, model)
+    }
+}
+
+const car = new Car('Toyota', 'Camry')
+const honda = new Honda('Honda', 'Civic')
+
+console.log("car instanceof Car:", car instanceof Car); // true
+console.log("honda instanceof Car:", honda instanceof Car); // true
+console.log("honda instanceof Car:", honda instanceof Vehicle); // false
+
+const anonFunc = () => {
+    return `Anonymous Function`
+}
+
+
+console.log(anonFunc instanceof Function)//true
+console.log([1, 2, 3, 4] instanceof Array)//true
+console.log([1, 2, 3, 4] instanceof Object)//true
+console.log(new Date('2000-01-01') instanceof Date)//true
+
+
+const printDate = (date: Date | string) => {
+    if (date instanceof Date) {
+        console.log(date.toUTCString())
+    } else {
+        console.log(new Date(date).toUTCString())
+    }
+}
