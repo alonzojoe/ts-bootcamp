1163a470a9b9f0d9d451039cff7a2440c538a82b
alonzojoe <alonzojoenell2k15@gmail.com>
2025-05-28 16:55:59 +0800

    setup webpack.config

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,4 @@
-import { add, pickRandom } from './utils.js';
+import { add, pickRandom } from './utils';
 
 console.log('modules')
 const pr = [1, 2, 3, 4, 5]
--- src/type-declarations/typeDeclarations.ts
+++ src/type-declarations/typeDeclarations.ts
@@ -1,66 +1,66 @@
-import axios from "axios"
-import _ from "lodash"
+// import axios from "axios"
+// import _ from "lodash"
 
-console.clear()
-console.log('Type Declaration')
+// console.clear()
+// console.log('Type Declaration')
 
 
-const API_ENDPOINT = `https://jsonplaceholder.typicode.com`
+// const API_ENDPOINT = `https://jsonplaceholder.typicode.com`
 
-interface APITodo {
-    userId: number;
-    id: number;
-    title: string;
-    completed: boolean;
-}
+// interface APITodo {
+//     userId: number;
+//     id: number;
+//     title: string;
+//     completed: boolean;
+// }
 
-let todos: APITodo[] = []
+// let todos: APITodo[] = []
 
-const fetchTodo = async (id: number) => {
-    try {
-        const response = await axios.get<APITodo>(`${API_ENDPOINT}/todos/${id}`)
-        const { data } = response
-        printTodo(data)
-    } catch (error) {
-        if (error instanceof Error) {
-            console.log(`Error: ${error.message}`)
-        } else {
-            console.log(error)
-        }
-    }
-}
+// const fetchTodo = async (id: number) => {
+//     try {
+//         const response = await axios.get<APITodo>(`${API_ENDPOINT}/todos/${id}`)
+//         const { data } = response
+//         printTodo(data)
+//     } catch (error) {
+//         if (error instanceof Error) {
+//             console.log(`Error: ${error.message}`)
+//         } else {
+//             console.log(error)
+//         }
+//     }
+// }
 
-function printTodo(todo: APITodo) {
-    console.log('todo details:', todo)
-}
+// function printTodo(todo: APITodo) {
+//     console.log('todo details:', todo)
+// }
 
-fetchTodo(1)
+// fetchTodo(1)
 
-interface Post {
-    id: number;
-    title: string;
-    body: string;
-    userId: number;
-}
+// interface Post {
+//     id: number;
+//     title: string;
+//     body: string;
+//     userId: number;
+// }
 
-const fetchData = async <T,>(endpoint: string): Promise<T> => {
-    try {
-        const res = await axios.get<T>(endpoint)
-        return res.data
-    } catch (error) {
-        if (error instanceof Error) {
-            throw new Error(error?.message)
-            // throw error instanceof Error ? error : new Error("An error occurred while fetching data");
-        } else {
-            throw new Error(`An error occured while fetching data`)
-        }
-    }
-}
+// const fetchData = async <T,>(endpoint: string): Promise<T> => {
+//     try {
+//         const res = await axios.get<T>(endpoint)
+//         return res.data
+//     } catch (error) {
+//         if (error instanceof Error) {
+//             throw new Error(error?.message)
+//             // throw error instanceof Error ? error : new Error("An error occurred while fetching data");
+//         } else {
+//             throw new Error(`An error occured while fetching data`)
+//         }
+//     }
+// }
 
-const posts = fetchData<Post[]>(`${API_ENDPOINT}/posts`).then((data) => { //Replace Promise<T> with Promise<Post[]>
-    console.log('posts', data)
-}).catch((e) => {
-    console.log('error', e)
-})
+// const posts = fetchData<Post[]>(`${API_ENDPOINT}/posts`).then((data) => { //Replace Promise<T> with Promise<Post[]>
+//     console.log('posts', data)
+// }).catch((e) => {
+//     console.log('error', e)
+// })
 
 
