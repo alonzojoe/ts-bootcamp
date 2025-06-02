562754ac324a8a11872a0abbebb14702170ffe64
alonzojoe <alonzojoenell2k15@gmail.com>
2025-06-02 16:35:52 +0800

    generics filterKey by value

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -6,3 +6,32 @@ const pr = [1, 2, 3, 4, 5]
 const randomEl = pickRandom(pr)
 
 const summation = add(1, 3)
+
+
+console.log('Generics Activities')
+
+interface User {
+    name: string;
+    role: string
+}
+
+const users: User[] = [
+    { name: 'John', role: 'Client' },
+    { name: 'Jane', role: 'Admin' },
+    { name: 'Jude', role: 'Client' },
+]
+
+const filterByKeyValue = <T, K extends keyof T>(items: T[], key: K, value: T[K]) => {
+    return items.filter((item) => item[key] === value)
+}
+
+console.log('Filters array of objects by key and value')
+console.log(filterByKeyValue(users, 'role', 'Admin'))
+
+const apple = {
+    name: 'Apple',
+    category: 'Fruits',
+    price: 2.25
+}
+
+type Product = keyof typeof apple
