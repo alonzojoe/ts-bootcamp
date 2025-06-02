e5fffaf45515e11b991602cb600700234b53bb12
alonzojoe <alonzojoenell2k15@gmail.com>
2025-06-02 16:49:06 +0800

    feat: array reduce generic type with Record utility type

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -35,3 +35,35 @@ const apple = {
 }
 
 type Product = keyof typeof apple
+
+interface Student {
+    id: number;
+    name: string;
+    age: number;
+}
+
+const students: Student[] = [
+    { id: 1, name: 'Mae', age: 23 },
+    { id: 2, name: 'Jane', age: 24 },
+    { id: 3, name: 'Lal', age: 22 },
+    { id: 4, name: 'There', age: 23 },
+    { id: 5, name: 'For', age: 21 },
+    { id: 6, name: 'Nikki', age: 24 },
+];
+
+type GroupedT<Type> = Record<string, Type[]>
+
+const groupByProperty = <T, K extends keyof T>(arr: T[], property: K) => {
+    return arr.reduce((group, curr) => {
+        const key = curr[property] as string
+
+        if (!group[key]) group[key] = []
+
+        group[key].push(curr)
+
+        return group
+    }, {} as GroupedT<T>)
+}
+
+console.log('array reduce, group by age')
+console.log(groupByProperty(students, 'age'))
